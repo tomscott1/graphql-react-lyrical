bc217be8fbdcd808bafe69fe38fbdb4d47a402c7
Tom Scott <tomscott85@gmail.com>
2017-04-20 21:59:56 +1000

    fetch data from props

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className='collection-item'>
+          {song.title}
+        </li>
+      )
+    })
+  }
+
   render() {
+    if (this.props.data.loading) {
+      return (<div>Loading...</div>)
+    }
     return (
       <div>
-        List
+        <ul className='collection'>
+          {this.renderSongs()}
+        </ul>
       </div>
     )
   }
@@ -14,9 +30,10 @@ class SongList extends Component {
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
 `
 
-export default SongList
+export default graphql(query)(SongList)
